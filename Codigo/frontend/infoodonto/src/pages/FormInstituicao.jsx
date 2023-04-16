7c4050827345ab8cbc0d96adc4b005d0c272a099
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-04-16 10:35:21 -0300

    [UPDATE] Correção de bug cadastro de instituição

--- Codigo/frontend/infoodonto/src/pages/FormInstituicao.jsx
+++ Codigo/frontend/infoodonto/src/pages/FormInstituicao.jsx
@@ -8,6 +8,11 @@ import InputOptions from '../components/input/InputOptions'
  const FormInstituicao = () => {
 
     const listaTipos = ['Universidade', 'Clínica', 'Hospital']
+    const [tipo,setTipo] = useState("")
+
+    function handleTipoChange(valor){
+      setTipo(valor)
+    }
 
     function createInstituicao(e){
         e.preventDefault()
@@ -20,14 +25,11 @@ import InputOptions from '../components/input/InputOptions'
                 "name": document.getElementById("name").value,
                 "email": document.getElementById("email").value,
                 "senha": document.getElementById("senha").value,
-                "instituicao": {
-                  "name": "Universidade C",
-                  "tipo": "Universidade"
-                }
+                "tipo": tipo
             })
           })
           .then(resp => resp.json())
-          .then(alert("Dentista cadastrado!"))
+          .then(alert("Instituição cadastrada!"))
           .catch(err => console.error(err))
     }
 
@@ -42,7 +44,7 @@ import InputOptions from '../components/input/InputOptions'
             <div className="uni"><Input type="text" placeholder="Nome" id="name"/></div>
                 <div className="uni"><Input type="text" placeholder="Email" id="email"/></div>
                 <div className="uni"><Input type="text" placeholder="Senha" id="senha"/></div>
-                <div className="uni"><InputOptions type="options" name="Tipo" content={listaTipos} /></div>
+                <div className="uni"><InputOptions type="options" name="Tipo" onChange={handleTipoChange} content={listaTipos} /></div>
                 <div className="divButton">
                     <button type="submit" className="confirmar">Confirmar</button>
                 </div>
