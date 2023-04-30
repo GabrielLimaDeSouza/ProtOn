a6124a98d689be00eef3c49aaf01bc4868337eb3
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-30 15:55:18 -0300

    [ADD] Adição do middleware de autenticação do login #35

---
+++ Codigo/backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+const JWT = require('jsonwebtoken')
+const { promisify } = require('util')
+const { SECRET_KEY } = require('../auth/configs')
+
+const authenticationMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if(!authHeader) {
+        res.status(401).json({ error: "Token was not provided" })
+    }
+     
+    const [, token] = authHeader.split(" ")
+    
+    JWT.verify(token, SECRET_KEY, (err, decoded) => {
+        if(err)
+            res.status(401).json({ error: 'Token invalido' })
+    
+        req.userId = decoded.id
+        next()
+    })
+}
+
+module.exports = authenticationMiddleware
