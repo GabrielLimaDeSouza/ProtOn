5afd7e6987acd9970f04071c34059b7dfdc5147d
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-30 15:57:15 -0300

    [UPDATE] {Front} Adição de restrição de rotas de usuário não logado #35

--- Codigo/frontend/infoodonto/src/App.jsx
+++ Codigo/frontend/infoodonto/src/App.jsx
@@ -1,32 +1,14 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom"
 
-//Pages
-import FormPaciente from "./pages/FormPaciente";
-import FormDentista from "./pages/FormDentista";
-import FormInstituicao from "./pages/FormInstituicao"
-import SearchPaciente from "./pages/SearchPaciente";
-import HomeInstituicao from "./pages/HomeInstituicao";
-import EditDentista from "./pages/EditDentista";
+//* CSS
+import './App.css'
 
-export default function App() {
-
-
+function App() {
   return (
-    
-     <BrowserRouter>
-      <Routes>
-        
-          <Route exact path="/formPaciente" element={<FormPaciente/>} />
-          <Route exact path="/formDentista" element={<FormDentista/>} />
-          <Route exact path="/formInstituicao" element={<FormInstituicao/>} />
-          <Route exact path="/searchPaciente" element={<SearchPaciente/>}/>
-          <Route exact path="/homeInstituicao" element={<HomeInstituicao/>}/>
-          <Route exact path="/editDentista/:id" element={<EditDentista />} />
-
-
-      </Routes>
-    </BrowserRouter>
-    
+    <div className="App">
+        <Outlet />
+    </div>
   )
 }
+
+export default App
--- Codigo/frontend/infoodonto/src/main.jsx
+++ Codigo/frontend/infoodonto/src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
+import { LoginProvider } from './context/LoginContext'
+
+//* Rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//* Pages User
+import FormPaciente from "./pages/FormPaciente"
+import FormDentista from "./pages/FormDentista"
+import FormInstituicao from "./pages/FormInstituicao"
+import SearchPaciente from "./pages/SearchPaciente"
+import HomeInstituicao from "./pages/HomeInstituicao"
+import EditDentista from "./pages/EditDentista"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/formPaciente", element: <FormPaciente /> },
+      { path: "/formDentista", element: <FormDentista /> },
+      { path: "/formInstituicao", element: <FormInstituicao /> },
+      { path: "/searchPaciente", element: <SearchPaciente /> },
+      { path: "/homeInstituicao", element: <HomeInstituicao /> },
+      { path: "/editDentista/:id", element: <EditDentista /> }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoginProvider>
+      <RouterProvider router={ router } />
+    </LoginProvider>
   </React.StrictMode>,
 )
