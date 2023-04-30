e64b542d6c905d44a767c0896c6ad3b74f71f97f
Nikolas Louret <navlouret@sga.pucminas.br>
2023-04-30 15:52:55 -0300

    [ADD] Adição do controller de usuário #35

---
+++ Codigo/backend/models/Usuario.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+
+const usuarioSchema = new Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        senha: {
+            type: String,
+            required: true
+        },
+        user: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Paciente'
+        },
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Insituicao'
+        },
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Dentista'
+        }]
+    },
+    { timestemps: true }
+)
+
+const Usuario = mongoose.model("Usuario", usuarioSchema)
+
+module.exports = {
+    Usuario,
+    usuarioSchema
+}
