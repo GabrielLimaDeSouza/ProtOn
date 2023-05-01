ec50fcdb7e359902f6bc4d3dba4b42bf464c2318
Nikolas Louret <navlouret@sga.pucminas.br>
2023-05-01 19:40:19 -0300

    [ADD] Adição de rotas para usuário

---
+++ Codigo/backend/routes/usuarioRouter.js
@@ -0,0 +1,14 @@
+const router = require("express").Router()
+const usuarioController = require("../controllers/usuarioController")
+const authenticationMiddleware = require("../middlewares/auth")
+
+//! Middleware
+//router.use(authenticationMiddleware)
+
+// GET Usuario
+router.route("/usuario").get((req, res) => usuarioController.get(req, res))
+
+// GET ALL Usuario
+router.route("/usuarios").get((req, res) => usuarioController.getAll(req, res))
+
+module.exports = router;
