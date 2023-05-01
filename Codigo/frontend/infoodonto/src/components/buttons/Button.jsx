801c5c836c737dd7d8857e628b4d04775a71ce57
Nikolas Louret <navlouret@sga.pucminas.br>
2023-05-01 19:41:01 -0300

    [ADD] Adição de componente para button

---
+++ Codigo/frontend/infoodonto/src/components/buttons/Button.jsx
@@ -0,0 +1,15 @@
+import './Button.css'
+
+const Button = ({ type, className, id, onClick, children }) => {
+    return (
+        <button type={ type }
+            className={ className && className }
+            id={ id }
+            onClick={ onClick && onClick }
+            >
+                { children }
+            </button>
+    )
+}
+
+export default Button
