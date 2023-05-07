c0a26ff3c19afabbbd1be9ae586d76384aba5f17
Nikolas Louret <navlouret@sga.pucminas.br>
2023-05-07 14:48:53 -0300

    [UPDATE] Remoção da ref de user e add typeCasting de user no get #35

--- Codigo/backend/models/Usuario.js
+++ Codigo/backend/models/Usuario.js
@@ -17,7 +17,6 @@ const usuarioSchema = new Schema(
             enum: ['paciente', 'dentista', 'instituicao'],
             required: true
         },
-        user: Schema.Types.ObjectId
     },
     { timestemps: true }
 )
