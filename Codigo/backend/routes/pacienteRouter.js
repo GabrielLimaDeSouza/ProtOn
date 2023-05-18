a63668b14167c3908825c5b72abb3e9be9fda5cb
Nikolas Louret <navlouret@sga.pucminas.br>
2023-05-18 12:11:17 -0300

    [UPDATE] Adição das rotas das solicitações

--- Codigo/backend/routes/pacienteRouter.js
+++ Codigo/backend/routes/pacienteRouter.js
@@ -1,26 +1,42 @@
-const router = require("express").Router()
-const pacienteController = require("../controllers/pacienteController")
-const authenticationMiddleware = require("../middlewares/auth")
+const router = require('express').Router()
+const pacienteController = require('../controllers/pacienteController')
+const solicitacaoController = require('../controllers/solicitacaoController')
+const authenticationMiddleware = require('../middlewares/auth')
 
 // CREATE Paciente
-router.route("/paciente").post((req, res) => pacienteController.create(req, res))
+router.route('/paciente').post((req, res) => pacienteController.create(req, res))
 
 //! Middleware
 //router.use(authenticationMiddleware)
 
 // GET Paciente
-router.route("/paciente").get((req, res) => pacienteController.get(req, res))
+router.route('/paciente').get((req, res) => pacienteController.get(req, res))
+
+// GET Dentistas Paciente
+router.route('/paciente/:id/dentistas').get((req, res) => pacienteController.dentistas(req, res))
 
 // GET ALL Paciente
-router.route("/pacientes").get((req, res) => pacienteController.getAll(req, res))
+router.route('/pacientes').get((req, res) => pacienteController.getAll(req, res))
 
 // GET BY CPF Paciente
-router.route("/paciente/cpf/:cpf").get((req, res) => pacienteController.getByCpf(req, res))
+router.route('/paciente/cpf/:cpf').get((req, res) => pacienteController.getByCpf(req, res))
 
 // DELETE Paciente
-router.route("/paciente").delete((req, res) => pacienteController.delete(req, res))
+router.route('/paciente').delete((req, res) => pacienteController.delete(req, res))
 
 // UPDATE Paciente
-router.route("/paciente").put((req, res) => pacienteController.update(req, res))
+router.route('/paciente').put((req, res) => pacienteController.update(req, res))
+
+// ENVIAR Solicitação Paciente
+router.route('/paciente/:id/solicitacoes').post((req, res) => solicitacaoController.enviarSolicitacao(req, res))
+
+// ACEITAR Solicitação Paciente
+router.route('/paciente/:id/solicitacoes').put((req, res) => solicitacaoController.aceitarSolicitacao(req, res))
+
+// GET Solicitação Paciente
+router.route('/paciente/:id/solicitacoes').get((req, res) => solicitacaoController.solicitacoes(req, res))
+
+// DELETE Solicitação Paciente
+router.route('/paciente/:id/solicitacoes').delete((req, res) => solicitacaoController.recusarSolicitacao(req, res))
 
 module.exports = router
