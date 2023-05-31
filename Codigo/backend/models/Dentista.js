9386d111f5c58743b6488f02b54b0f09f0bd7407
Nikolas Louret <navlouret@sga.pucminas.br>
2023-05-30 22:19:28 -0300

    [UPDATE] Atualização da matricula para unique

--- Codigo/backend/models/Dentista.js
+++ Codigo/backend/models/Dentista.js
@@ -1,31 +1,32 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const dentistaSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        matricula: {
-            type: String,
-            required: true
-        },
-        instituicao:{
-            type: Schema.Types.ObjectId,
-            ref: 'Instituicao'
-        },
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: 'Usuario'
-        }
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestemps: true }
-)
+    matricula: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    instituicao: {
+      type: Schema.Types.ObjectId,
+      ref: "Instituicao",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "Usuario",
+    },
+  },
+  { timestamps: true }
+);
 
-const Dentista = mongoose.model("Dentista", dentistaSchema)
+const Dentista = mongoose.model("Dentista", dentistaSchema);
 
 module.exports = {
-    Dentista,
-    dentistaSchema
-}
+  Dentista,
+  dentistaSchema,
+};
