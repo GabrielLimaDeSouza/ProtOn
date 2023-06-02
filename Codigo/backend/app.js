c8a1a5296a38c35b94456b9a1a8335bf21464e72
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-06-02 17:57:50 -0300

    [UPDATE] Rotas Deploy

--- Codigo/backend/app.js
+++ Codigo/backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require('path');
 const cors = require("cors")
 const app = express()
 
@@ -13,5 +14,11 @@ conn()
 //* Routes
 const routes = require("./routes/router")
 app.use("/api", routes)
+
+app.use(express.static(path.join(__dirname, '../../frontend/infoodonto/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/infoodonto/build', 'index.html'));
+});
 
 app.listen(3000, () => console.log("Servidor ONLINE"))
