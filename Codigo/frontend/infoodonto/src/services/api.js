e1be386923527b346cf80400f66ee54e1970f992
Nikolas Louret <navlouret@sga.pucminas.br>
2023-06-14 18:39:44 -0300

    [UPDATE] Correção dos btns das condições

--- Codigo/frontend/infoodonto/src/services/api.js
+++ Codigo/frontend/infoodonto/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://protonbackend.onrender.com",
+  baseURL: "http://localhost:3000",
 });
 
 // http://localhost:3000
